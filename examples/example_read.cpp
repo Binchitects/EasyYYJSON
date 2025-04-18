34b099dc97a38dfb98084a2b4f7f66c436918e73
Ali Ghadyani <ali.ghadyani.1379@gmail.com>
2025-04-18 23:07:57 +0330

    init project

---
+++ examples/example_read.cpp
@@ -0,0 +1,7 @@
+extern "C" {
+#include <yyjson.h>
+}
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
---
+++ include/easy_yyjson.hpp
@@ -0,0 +1,8 @@
+#ifndef __EASY_YYJSON_HPP__
+#define __EASY_YYJSON_HPP__
+
+namespace EasyYYJSON {
+    
+}
+
+#endif // __EASY_YYJSON_HPP__
